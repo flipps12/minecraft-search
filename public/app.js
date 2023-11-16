13912a4e580f649370ddc2e51f74744a395290c9
Flipps <paginasypruebas12@gmail.com>
2023-11-15 22:07:57 -0300

    0.2

--- public/app.js
+++ public/app.js
@@ -1,46 +1,75 @@
-const container = document.getElementById('container');
-const version = document.getElementById('version');
-const players = document.getElementById('players');
-const html = document.getElementById('html');
-const img = document.getElementById('img');
-const nameServer = document.getElementById('name');
 const loading = document.getElementById('loading');
+var key = 1;
+
+const crearElemento = (tag, clase, id) => {
+  const elemento = document.createElement(tag);
+  elemento.className = clase;
+  elemento.id = `${id}${key}`; // Asumiendo que "numero" es una variable con el valor deseado
+  return elemento;
+}
+
+crearElementoHTML = () => {
+  let serverContainer = document.querySelector('.server');
+  let container = crearElemento('div', 'container', 'container');
+  let version = crearElemento('div', 'version', 'version');
+  let container2 = crearElemento('div', 'container2', 'container');
+  let img = crearElemento('img', 'img', 'img');
+  let subContainer = crearElemento('div', 'sub-container', 'sub-container');
+  let subContainer1 = crearElemento('div', 'sub-container1', 'sub-container');
+  let name = crearElemento('div', 'name', 'name');
+  let players = crearElemento('div', 'players', 'players');
+  let subContainer2 = crearElemento('div', 'sub-container2', 'sub-container');
+  let html = crearElemento('div', 'html', 'html');
+
+  subContainer1.append(name, players);
+  subContainer2.appendChild(html);
+  subContainer.append(subContainer1, subContainer2)
+  container2.append(img, subContainer);
+  container.append(version, document.createElement('br'), container2);
+
+  serverContainer.appendChild(container);
+}
+
 const writeHtml = (data) => {
-  if (data.mensaje !== null){
-    version.textContent = data.mensaje.version.name
-    players.textContent = `${data.mensaje.players.online}/${data.mensaje.players.max}`
-    html.innerHTML = data.mensaje.motd.html
-    img.src = data.mensaje.favicon
-    nameServer.textContent = data.mensaje.motd.clean
+  if (data.mensaje !== null) {
+    document.getElementById(`version${key}`).textContent = data.mensaje.version.name
+    document.getElementById(`players${key}`).textContent = `${data.mensaje.players.online}/${data.mensaje.players.max}`
+    document.getElementById(`html${key}`).innerHTML = data.mensaje.motd.html
+    document.getElementById(`img${key}`).src = data.mensaje.favicon
+    document.getElementById(`name${key}`).textContent = data.mensaje.motd.clean
   } else {
-    nameServer.textContent = 'No se encuentran servidores con esta ip o este puerto'
-    version.textContent = '' 
-    players.textContent = ''
-    html.innerHTML = '' 
-    img.src = ''
+    document.getElementById(`name${key}`).textContent = 'No se encuentran servidores con esta ip o este puerto'
+    document.getElementById(`img${key}`).src = ''
   }
 }
 document.getElementById('Form').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    // Obtener los valores del formulario
-    const username = document.getElementById('ip').value;
-    loading.removeAttribute('hidden')
-    // Enviar los datos al servidor mediante una solicitud POST
-    fetch('/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username }),
-    })
+  event.preventDefault();
+  const username = document.getElementById('ip').value;
+
+  // Obtener los valores del formulario
+  minecraftSearch(username)
+});
+
+const minecraftSearch = (username) => {
+  loading.removeAttribute('hidden')
+  // Enviar los datos al servidor mediante una solicitud POST
+  fetch('/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username }),
+  })
     .then(response => response.json())
     .then(data => {
-      writeHtml(data)
-      loading.setAttribute('hidden','')      
+      crearElementoHTML()
+      writeHtml(data, key)
+      key++
+      loading.setAttribute('hidden', '')
       // Puedes realizar acciones adicionales aquí según la respuesta del servidor
     })
     .catch(error => {
       console.error('Error al enviar la solicitud:', error);
     });
-  });
+}
+minecraftSearch('mc.universocraft.com')
